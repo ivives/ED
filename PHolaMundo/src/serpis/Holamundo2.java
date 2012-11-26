674fe7cf517f3ce00d9d5d38dee3c8f4e45db9db
Ignacio Vives <ivives75@hotmail.com>
2012-11-26 11:50:19 +0100

    Añadiendo PHolaMundo a GIT

---
+++ PHolaMundo/src/serpis/Holamundo2.java
@@ -0,0 +1,14 @@
+package serpis;
+
+public class Holamundo2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hola desde Java 2"); 
+	}
+
+}
