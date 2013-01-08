c6cf5a7c9cf1f48c68c02a0ab9088404b8897803
Ignacio Vives <ivives75@hotmail.com>
2013-01-08 13:10:31 +0100

    Añadido PTest Alumno

---
+++ PTest/src/serpis/ed/Alumno.java
@@ -0,0 +1,20 @@
+package serpis.ed;
+
+public class Alumno {
+
+	private String nombre;
+	
+	public void setNombre(String nombre){
+		this.nombre = nombre;
+	}
+	
+	public String getNombre(){
+		return nombre;
+	}
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
